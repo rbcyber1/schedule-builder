214ef39b48cf19aff4e519c04374598e5506c379
bqbbo <bobbye23899@gmail.com>
2026-02-15 16:22:49 -0800

    fix es6 stuff hopefully should work now

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,14 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/.env" });
 
+const BACKEND_PORT = process.env.BACKEND_PORT || 8000;
+
 export default defineConfig({
     plugins: [react()],
     server: {
         proxy: {
             "/api": {
-                target: `http://localhost:${process.env.BACKEND_PORT}`,
+                target: `http://localhost:${BACKEND_PORT}`,
                 changeOrigin: true,
             },
         },
